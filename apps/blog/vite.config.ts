608d3bddecb32cc5d9b82baf26a6a4cc5fb69537
nl-wl <bui-lam@web-life.co.jp>
2025-09-24 16:10:52 +0700

    fix: deploy netlify

--- apps/blog/vite.config.ts
+++ apps/blog/vite.config.ts
@@ -2,15 +2,19 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { installGlobals } from "@remix-run/node";
 import { defineConfig, type PluginOption } from "vite";
 import { vercelPreset } from "@vercel/remix/vite";
+import { netlifyPlugin } from "@netlify/remix-adapter/plugin";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 installGlobals();
 
 export default defineConfig(() => {
   const isVitest = process.env.VITEST;
+  const isNetlify = !!process.env.NETLIFY;
   const plugins: PluginOption[] = [
     // Disable Remix plugin in Vitest to avoid preamble errors
     !isVitest && remix({ presets: [vercelPreset()] }),
+    // When building on Netlify, apply its adapter plugin
+    isNetlify && netlifyPlugin(),
     tsconfigPaths(),
   ].filter(Boolean) as PluginOption[];
 
