3d17f3c5458dcabd90c132756ce0c8ed91aa41b3
nl-wl <bui-lam@web-life.co.jp>
2025-09-24 16:22:32 +0700

    fix: deploy netlify

---
+++ apps/blog/.netlify/functions-internal/remix-server.mjs
@@ -0,0 +1,10 @@
+
+    export { default } from "../../build/server/nodejs-eyJydW50aW1lIjoibm9kZWpzIn0/server.js";
+
+    export const config = {
+      name: "Remix server handler",
+      generator: "@netlify/remix-adapter@2.6.1",
+      path: "/*",
+      preferStatic: true,
+    };
+    
